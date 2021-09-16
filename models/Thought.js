4965fbe2314ee1dcd6d44ca2c0c66144021391af
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-09-16 06:05:38 -0500

    Add Reaction schema to Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,34 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
@@ -17,12 +45,8 @@ const ThoughtSchema = new Schema(
       type: String,
       required: true,
     },
-    reaction: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Reaction'
-      }
-    ],
+    // use ReactionSchema for reaction validation
+    reaction: [ReactionSchema],
   },
   {
     toJSON: {
